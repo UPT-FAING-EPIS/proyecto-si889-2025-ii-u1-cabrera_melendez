5f45e52167bad4ebaf1eee2a2b1f131d107bfb1c
ccabrerastu <camcabrera@upt.pe>
2025-09-10 11:18:01 -0500

    Release #16

--- simulapp/lib/services/google_auth.dart
+++ simulapp/lib/services/google_auth.dart
@@ -10,7 +10,7 @@ const List<String> scopes = <String>[
 ];
 
 class GoogleAuth {
-  final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: scopes);
+  final GoogleSignIn _googleSignIn = GoogleSignIn.withScopes(scopes);
   final StreamController<GoogleSignInAccount?> _userController =
       StreamController.broadcast();
   final StreamController<bool> _authorizationController =
@@ -39,7 +39,7 @@ class GoogleAuth {
   Future<String?> signIn() async {
     try {
       final GoogleSignInAccount? user = await _googleSignIn.signIn();
-      
+
       if (user == null) {
         // El usuario canceló el inicio de sesión
         return 'Inicio de sesión con Google cancelado.';
@@ -72,7 +72,8 @@ class GoogleAuth {
       }
     } on FirebaseAuthException catch (e) {
       // Errores específicos de Firebase Auth
-      print('Error de Firebase Auth durante el inicio de sesión con Google: ${e.code} - ${e.message}');
+      print(
+          'Error de Firebase Auth durante el inicio de sesión con Google: ${e.code} - ${e.message}');
       return 'Error de autenticación: ${e.message}';
     } catch (error) {
       // Otros errores generales
@@ -83,15 +84,19 @@ class GoogleAuth {
 
   /// Crea o actualiza el documento del usuario en Firestore.
   Future<void> _createUserInFirestore(User user) async {
-    final userDocRef = FirebaseFirestore.instance.collection('users').doc(user.uid);
+    final userDocRef =
+        FirebaseFirestore.instance.collection('users').doc(user.uid);
     final userDoc = await userDocRef.get();
 
     if (!userDoc.exists) {
       // Si el documento no existe, crearlo
       await userDocRef.set({
         'email': user.email,
-        'username': user.displayName ?? user.email?.split('@')[0] ?? 'No username', // Intenta usar el nombre de usuario de Google, o parte del email
-        'createdAt': FieldValue.serverTimestamp(), // Usa un timestamp del servidor
+        'username': user.displayName ??
+            user.email?.split('@')[0] ??
+            'No username', // Intenta usar el nombre de usuario de Google, o parte del email
+        'createdAt':
+            FieldValue.serverTimestamp(), // Usa un timestamp del servidor
         'photoUrl': user.photoURL,
       });
     } else {
