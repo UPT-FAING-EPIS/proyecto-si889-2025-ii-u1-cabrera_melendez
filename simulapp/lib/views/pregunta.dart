5381c7349659524125ec81c334574150de82f22d
GabrielFMH <84587604+GabrielFMH@users.noreply.github.com>
2025-11-27 14:34:38 -0500

    boton fix

--- simulapp/lib/views/pregunta.dart
+++ simulapp/lib/views/pregunta.dart
@@ -196,27 +196,31 @@ class _ExamenScreenContentState extends State<_ExamenScreenContent> {
                 }).toList(),
               ),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                print(
-                    'Botón "Enviar respuesta" presionado. Índice: ${viewModel.currentQuestionIndex}');
-                if (viewModel.respuestaSeleccionada != null) {
-                  viewModel.evaluarRespuesta();
-                } else {
-                  print('No se seleccionó ninguna respuesta');
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.lightBlue,
-              ),
-              child: const Text('Enviar respuesta',
-                  style: TextStyle(color: Colors.lightBlue)),
+            const SizedBox(height: 0),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (viewModel.respuestaSeleccionada != null) {
+                        viewModel.evaluarRespuesta();
+                      } else {
+                        print('No se seleccionó ninguna respuesta');
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.lightBlue,
+                    ),
+                    child: const Text('Enviar respuesta',
+                        style: TextStyle(color: Colors.lightBlue)),
+                  ),
+                ),
+                const SizedBox(width: 16.0),
+                Text('Puntaje: ${viewModel.puntaje.toStringAsFixed(2)}',
+                    style: const TextStyle(color: Colors.black)),
+              ],
             ),
-            const SizedBox(height: 20),
-            Text('Puntaje: ${viewModel.puntaje.toStringAsFixed(2)}',
-                style: const TextStyle(color: Colors.black)),
           ],
         ),
       ),
